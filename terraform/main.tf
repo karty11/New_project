a4316322c00233f98e6494b581945a7ea6219bb9
karty11 <karthikperumal11@gmail.com>
2025-09-07 19:16:48 +0530

    ci and cd updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ data "tls_certificate" "oidc" {
 resource "aws_iam_openid_connect_provider" "eks" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc.sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.oidc.certificates[0].sha1_fingerprint]
 }
 # }
 # resource "aws_iam_openid_connect_provider" "eks" {
