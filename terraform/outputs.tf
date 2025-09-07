dcc49c208091db20556d8289dd284c4095f79ef5
karty11 <karthikperumal11@gmail.com>
2025-09-07 17:44:28 +0530

    ci and cd updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,11 @@ output "oidc_provider" {
 output "cluster_name_debug" {
   value = var.cluster_name
 }
+
+output "aws_region_debug" {
+  value = var.aws_region
+}
+
+output "aws_identity_debug" {
+  value = data.aws_caller_identity.current.arn
+}
