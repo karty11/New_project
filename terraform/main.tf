bc74889046f38951fcdcfa2c5e4cef1db8354fd4
karty11 <karthikperumal11@gmail.com>
2025-09-07 17:25:33 +0530

    ci and cd updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,11 @@ data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = var.cluster_name
+}
+
+data "aws_caller_identity" "current" {}
 
 locals {
   oidc_issuer = replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")
